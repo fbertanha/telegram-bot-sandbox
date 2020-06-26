690d01966e7d76f45e36417780e47b8064b8f89f
felipe.bertanha <felipe.bertanha@capitaletecnologia.com.br>
2020-06-25 21:22:58 -0300

    removido arquivos n usados alterado index.js

--- index.js
+++ index.js
@@ -10,14 +10,6 @@ const bot = new TelegramBot(token, {polling: true});
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
 
-    //linha teste outro pc
-
-
-    //1
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-
     const chatId = msg.chat.id;
     const resp = match[1]; // the captured "whatever"
 
